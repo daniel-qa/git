7e17a50cd5326b4924f77982f91d829a00d1d184
daniel <daniel.lin.gr@gmail.com>
2024-05-27 15:35:58 +0800

    Update git_sort_commit_sha.py

--- 列出版本差異/get_short_commit_sha/git_sort_commit_sha.py
+++ 列出版本差異/get_short_commit_sha/git_sort_commit_sha.py
@@ -16,6 +16,11 @@ def get_commit_one_week_ago():
     command = f"git log -1 --before='{one_week_ago}' --pretty=format:%h"
     return run_git_command(command)
 
+def get_commit_one_month_ago():
+    one_month_ago = (datetime.now() - timedelta(days=30)).isoformat()
+    command = f"git log -1 --before='{one_month_ago}' --pretty=format:%h"
+    return run_git_command(command)
+
 if __name__ == "__main__":
     try:
         latest_commit = get_latest_commit()
